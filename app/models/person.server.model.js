d83f08231cc9eb86700f7bfe6505b370be549189
nbuechler <natebuechler@gmail.com>
2014-11-11 08:28:39 -0500

    Added a people section to the app, start for more... have to go to job

---
+++ app/models/person.server.model.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Person Schema
+ */
+var PersonSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Person name',
+		trim: true
+	},
+    pob: {
+		type: String,
+		default: '',
+		required: 'Please fill Person Place of Birth',
+		trim: true
+	},
+    /*
+    dob: {
+		type: String,
+		default: '',
+		required: 'Please fill Person Date of Birth',
+		trim: true
+	},
+    */
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Person', PersonSchema);
